9f8bfb180be0ffce39d12e7504d1c43279227ecb
giuliozaccardo <94835547+giuliozaccardo@users.noreply.github.com>
2026-03-25 17:08:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,5 +34,6 @@ if ingredients_list:
 import requests  
 # ✅ CORRETTO (solo l'URL tra virgolette)
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
