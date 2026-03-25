999cb39809b43df7376a0254b0d67b985899255e
giuliozaccardo <94835547+giuliozaccardo@users.noreply.github.com>
2026-03-25 17:06:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,7 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+# ✅ CORRETTO (solo l'URL tra virgolette)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
 st.text(smoothiefroot_response)
 
