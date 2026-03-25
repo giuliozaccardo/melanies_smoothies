621c0be738c7c48d1e51a68796b4b1a29be70132
giuliozaccardo <94835547+giuliozaccardo@users.noreply.github.com>
2026-03-25 17:16:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,15 +21,18 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
+
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
+        # 2. CERCHIAMO IL VALORE SEARCH_ON CORRISPONDENTE AL FRUTTO SCELTO
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + ' Nutrition Information')
         
-        # AGGIUNTA LA PARENTESI TONDA ALLA FINE )
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        
-        # Visualizza i dati
+        # 3. USIAMO search_on NELL'API, NON fruit_chosen
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
